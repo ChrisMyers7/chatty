3c1efa6393e0ca2b200c3a9d1ee88d34f9c248e1
Chris Myers <7chrismmyers7@gmail.com>
2016-06-08 16:11:22 -0600

    finished projects

--- server.js
+++ server.js
@@ -1,1 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express()
+app.use(bodyParser.json());
 
+var messages = [];
+
+var port = 8000;
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }).send(JSON.stringify(messages));
+})
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+  messages.push({
+    message: req.body.message,
+    time: new Date()
+  });
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+})
+
+
+app.listen(port, function() {
+  console.log('listening to ', port);
+})
